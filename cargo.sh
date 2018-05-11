591f01fbd7f66d851fc2f04247c5eb8238b5a9ef
wkfok <wkfok@VAIO>
2018-05-11 17:58:56 +0800

    Update cargo.sh

--- cargo.sh
+++ cargo.sh
@@ -1,13 +1,8 @@
 #!/bin/sh
-
 cd /opt/source/news-extractor
 nohup mvn cargo:run > cargo.out &
-
-( tail -f -n0 cargo.out & ) | grep -q "started on port \[8080\]"
+echo $! > cargo.pid
 
 if [[ -e /opt/source/twitter4j.properties ]] ; then
-cp /opt/source/twitter4j.properties /opt/source/news-extractor/src/main/
+	cp /opt/source/twitter4j.properties /opt/source/news-extractor/src/main/
 fi
-#curl localhost:8080/demo
-#curl localhost:8080/twitterStream
-echo "started"
