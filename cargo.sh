00017a52c490f3add02156d2d21dc10ef857c5af
wkfok <wkfok@VAIO>
2018-05-11 15:55:12 +0800

    cargo.sh

--- cargo.sh
+++ cargo.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
+
 cd /opt/source/news-extractor
 nohup mvn cargo:run > cargo.out &
 
-curl localhost:8080/demo
-curl localhost:8080/twitterStream
+( tail -f -n0 cargo.out & ) | grep -q "started on port \[8080\]"
+
+#curl localhost:8080/demo
+#curl localhost:8080/twitterStream
+echo "started"
